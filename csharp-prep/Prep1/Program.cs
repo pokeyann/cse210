8e68827b082e113d75abf8de0ae44bc14d42bc04
pokeyann <pokeyann@gmail.com>
2023-01-12 21:57:02 -0600

    Added James Bond Name Code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,16 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    /*Console.WriteLine("Hello Prep1 World!");*/
+
+    Console.Write("What is your first name? ");
+    String fname = Console.ReadLine();
+
+    Console.Write("What is your last name? ");
+    String lname = Console.ReadLine();
+
+    Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+  }
 }
