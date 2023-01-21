7e7f23fd2338143d8f1c453b9cb9fbd8ee9d9ac4
pokeyann <pokeyann@gmail.com>
2023-01-20 23:59:13 -0600

    Prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,50 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.Write("Please enter your grade percentage:  ");
+    string gradePercent = Console.ReadLine();
+    int answer = int.Parse(gradePercent);
+
+    string letter = "";
+
+    if (answer >= 90)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      letter = "A";
     }
+
+    else if (answer >= 80)
+    {
+      letter = "B";
+    }
+
+    else if (answer >= 70)
+    {
+      letter = "C";
+    }
+
+    else if (answer >= 60)
+    {
+      letter = "D";
+    }
+
+    else
+    {
+      letter = "F";
+    }
+
+    Console.WriteLine($"Your letter grade is {letter}");
+
+    if (answer >= 70)
+    {
+      Console.WriteLine("Congratulations, you passed!");
+    }
+
+    else
+    {
+      Console.WriteLine("You did not pass, better luck next time.");
+    }
+  }
 }
+
