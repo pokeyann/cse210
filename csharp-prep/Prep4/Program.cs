c114e8ac0b3c108d2565bf894339a5420ab91195
pokeyann <pokeyann@gmail.com>
2023-01-27 17:01:56 -0600

    list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    List<int> numbers = new List<int>();
+
+    Console.WriteLine("Hello Prep4 World!");
+
+    Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+    int userNumber = -1;
+    do
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+
+      Console.WriteLine("Enter a number: ");
+
+      string userResponse = Console.ReadLine();
+
+      userNumber = int.Parse(userResponse);
+
+    } while (userNumber != 0);
+  }
 }
