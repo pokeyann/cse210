ea353f32fb1978b25989bde624bcdea330211a9f
pokeyann <pokeyann@gmail.com>
2023-01-27 16:36:54 -0600

    prep 3 guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,38 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep3 World!");
+
+    Random randomGenerator = new Random();
+    int magicNumber = randomGenerator.Next(1, 100);
+
+    int guess = -1;
+
+    while (guess != magicNumber)
     {
-        Console.WriteLine("Hello Prep3 World!");
+      Console.WriteLine("What is your guess?");
+      guess = int.Parse(Console.ReadLine());
+
+      if (magicNumber < guess)
+      {
+        Console.WriteLine("Lower");
+      }
+
+      else if (magicNumber > guess)
+      {
+        Console.WriteLine("Higher");
+      }
+
+      else
+      {
+        Console.WriteLine("You guessed it!");
+      }
     }
+  }
 }
+
+
+/*Console.WriteLine("What is the magic number?");
+      int magicNumber = int.Parse(Console.ReadLine());*/
