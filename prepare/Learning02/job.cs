f6a07045d986dad8f2a53bbdd6259291ad3d2160
pokeyann <pokeyann@gmail.com>
2023-01-28 22:34:32 -0600

    create job class

---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+  public class Job
+  {
+    public String _jobTitle = "";
+    public String _company = "";
+    public String _startYear = "";
+    public String _endYear = "";
+    public Job()
+       public JobInformation()
+    {
+      Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} - {_endYear}");
+    }
+  }
+}
