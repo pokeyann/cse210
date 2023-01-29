11ffd5678969e5d180af62d363ea497b48f7187c
pokeyann <pokeyann@gmail.com>
2023-01-28 23:13:41 -0600

    create job1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,15 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Learning02 World!");
+
+    Job job1 = new Job();
+    job1._jobTitle = "Data Developer";
+    job1._company = "Data World";
+    job1._startYear = "2006";
+    job1._endYear = "2020";
+    Console.WriteLine(job1._jobTitle);
+  }
 }
