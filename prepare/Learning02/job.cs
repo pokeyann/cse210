132a6a6188fe76cc09638c8dbe58091d5ad3c155
pokeyann <pokeyann@gmail.com>
2023-01-28 22:39:55 -0600

    fixed errors

--- prepare/Learning02/job.cs
+++ prepare/Learning02/job.cs
@@ -1,17 +1,13 @@
 using System;
 
-class Program
+public class Job
 {
-  public class Job
+  public String _jobTitle = "";
+  public String _company = "";
+  public String _startYear = "";
+  public String _endYear = "";
+  public void JobInformation()
   {
-    public String _jobTitle = "";
-    public String _company = "";
-    public String _startYear = "";
-    public String _endYear = "";
-    public Job()
-       public JobInformation()
-    {
-      Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} - {_endYear}");
-    }
+    Console.WriteLine($"{_jobTitle} ({_company}) {_startYear} - {_endYear}");
   }
 }
