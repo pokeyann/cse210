ffec2c90a50934ef6a13bd4bbb29ff849dfce044
pokeyann <pokeyann@gmail.com>
2023-01-28 23:17:07 -0600

    create job2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -12,5 +12,12 @@ class Program
     job1._startYear = "2006";
     job1._endYear = "2020";
     Console.WriteLine(job1._jobTitle);
+
+    Job job2 = new Job();
+    job2._jobTitle = "Database Administrator";
+    job2._company = "King of the MySql Castle";
+    job2._startYear = "1994";
+    job2._endYear = "1997";
+    Console.WriteLine(job2._jobTitle);
   }
 }
